8d23e1d913d476f401d12bc4ff381f1bb82ff2be
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-04-02 23:47:51 -0400

    CORS not resolved

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
-  async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`
-        }
-      ]
-    };
-  },
   async headers() {
     return [
       {
@@ -39,4 +25,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
